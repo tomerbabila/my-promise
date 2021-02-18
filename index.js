d001aad3d977cf868c45c1b64349bf16f18cf14c
tomertv2 <tomertv2@gmail.com>
2021-02-18 20:46:23 +0200

    Add resolve and reject methods

--- index.js
+++ index.js
@@ -9,5 +9,15 @@ class MyPromise {
     this.state = states.pending;
     this.value = undefined;
     this.handlers = [];
+
+    const resolve = (result) => {
+      this.state = states.fulfilled;
+      this.value = result;
+    };
+
+    const reject = (error) => {
+      this.state = states.rejected;
+      this.value = error;
+    };
   }
 }
